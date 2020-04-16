7147fff1bbbaa7fce09f53cca5e90c69c1a86348
ahmed_elbassel <elbassel.n13@gmail.com>
2020-04-16 19:38:26 +0200

    fix linting

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ LOG.setLevel(logging.INFO)
 def scale(payload):
     """Scales Payload"""
     
-    LOG.info(f"Scaling Payload: \n{payload}")
+    # LOG.info(f"Scaling Payload: \n{payload}")
+    LOG.info('Scaling Payload: \n%s', payload)
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
